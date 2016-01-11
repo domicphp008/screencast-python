542d049df329220550a6b625779a5bf5ebb2aab2
Domicio <testedomicio@hotmail.com>
2016-01-10 23:35:10 -0200

    random

---
+++ RandomMaisImportes.py
@@ -0,0 +1,16 @@
+import random
+
+#random.randrange(start, stop[, step])
+"""
+Retorna aleatæoriamente um elemeno de range (começo, fim passo).
+Isso é equivalente a choice(range(começo, fim, passo)),mas não constroi um
+objeto range.
+"""
+#Exemplo
+"""
+for i in range(10):
+    #x = random.randrange(1,7)
+    #x = random.randrange(1, 7, 2)
+    print(x)
+"""
+#####################################################################
