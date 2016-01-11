d20d0fdb6b2d3bb515bc0df1482bb784cbaf29b0
Domicio <testedomicio@hotmail.com>
2016-01-11 00:26:23 -0200

    Aqui finaliza exemplo de random
    Quatro exemplo

---
+++ randomb.py
@@ -0,0 +1,16 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: /Users/domiciosilva/Documents/screencast-python/randomA.py ====
+7
+3
+3
+2
+4
+5
+1
+6
+6
+3
+>>> 
